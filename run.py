b65c550003e59ce45939b8f4cb124b832a07776a
Mike <rans@un.org>
2021-08-20 14:50:59 +1200

    Fix for WHERETOSTART=RESET

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from os.path import join, expanduser
 
 from hdx.hdx_configuration import Configuration
 from hdx.utilities.downloader import Download
-from hdx.utilities.path import progress_storing_tempdir, get_temp_dir
+from hdx.utilities.path import wheretostart_tempdir_batch, progress_storing_folder
 
 from faostat import generate_dataset_and_showcase, download_indicatorsets, get_countries
 
@@ -28,22 +28,24 @@ def main():
     indicatorsetnames = Configuration.read()['indicatorsetnames']
     showcase_base_url = Configuration.read()['showcase_base_url']
     with Download() as downloader:
-        folder = get_temp_dir('FAOSTAT')
-        indicatorsets = download_indicatorsets(filelist_url, indicatorsetnames, downloader, folder)
-        logger.info('Number of indicator types to upload: %d' % len(indicatorsetnames))
-        countries, countrymapping = get_countries(countrygroup_url, downloader)
-        logger.info('Number of countries to upload: %d' % len(countries))
-        for info, country in progress_storing_tempdir('FAOSTAT', countries, 'iso3'):
-            for indicatorsetname in indicatorsets:
-                dataset, showcase, bites_disabled, qc_indicators = generate_dataset_and_showcase(
-                    indicatorsetname, indicatorsets, country, countrymapping, showcase_base_url, filelist_url,
-                    downloader, info['folder'])
-                if dataset:
-                    dataset.update_from_yaml()
-                    dataset.generate_resource_view(-1, bites_disabled=bites_disabled, indicators=qc_indicators)
-                    dataset.create_in_hdx(remove_additional_resources=True, hxl_update=False, updated_by_script='HDX Scraper: FAOStat', batch=info['batch'])
-                    showcase.create_in_hdx()
-                    showcase.add_dataset(dataset)
+        with wheretostart_tempdir_batch(lookup) as info:
+            folder = info['folder']
+            batch = info['batch']
+            indicatorsets = download_indicatorsets(filelist_url, indicatorsetnames, downloader, folder)
+            logger.info('Number of indicator types to upload: %d' % len(indicatorsetnames))
+            countries, countrymapping = get_countries(countrygroup_url, downloader)
+            logger.info('Number of countries to upload: %d' % len(countries))
+            for info, country in progress_storing_folder(info, countries, 'iso3'):
+                for indicatorsetname in indicatorsets:
+                    dataset, showcase, bites_disabled, qc_indicators = generate_dataset_and_showcase(
+                        indicatorsetname, indicatorsets, country, countrymapping, showcase_base_url, filelist_url,
+                        downloader, info['folder'])
+                    if dataset:
+                        dataset.update_from_yaml()
+                        dataset.generate_resource_view(-1, bites_disabled=bites_disabled, indicators=qc_indicators)
+                        dataset.create_in_hdx(remove_additional_resources=True, hxl_update=False, updated_by_script='HDX Scraper: FAOStat', batch=batch)
+                        showcase.create_in_hdx()
+                        showcase.add_dataset(dataset)
 
 
 if __name__ == '__main__':
