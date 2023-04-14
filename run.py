4ff50a42e3ae64376c4dcf34d5d5d9c47dda6677
Mike <rans@un.org>
2023-04-14 17:38:39 +1200

    Update QuickCharts

--- run.py
+++ run.py
@@ -56,7 +56,7 @@ def main():
                     )
                     if dataset:
                         dataset.update_from_yaml()
-                        dataset.generate_resource_view(
+                        dataset.generate_quickcharts(
                             -1, bites_disabled=bites_disabled, indicators=qc_indicators
                         )
                         dataset.create_in_hdx(
